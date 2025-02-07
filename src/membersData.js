0a29455753409d1d229733ce169471247c244528
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-08 01:46:52 +0530

    Create membersData.js

---
+++ src/membersData.js
@@ -0,0 +1,31 @@
+  // Full members data
+  export const members = {
+    current: {
+      postdoc: [
+        { name: "Postdoc 1", year: "2023-present", research: "Beyond Standard Model" },
+        { name: "Postdoc 1", year: "2023-present", research: "Beyond Standard Model" },
+        { name: "Postdoc 1", year: "2023-present", research: "Beyond Standard Model" }
+      ],
+      phd: [
+        { name: "PhD Scholar 1", advisor: "Prof. Amruta Mishra", year: "2022-present", research: "Strongly Interacting Matter" },
+        { name: "PhD Scholar 2", advisor: "Prof. Amruta Mishra", year: "2021-present", research: "Hadron Properties" },
+        { name: "PhD Scholar 2", advisor: "Prof. Amruta Mishra", year: "2021-present", research: "Hadron Properties" },
+        { name: "PhD Scholar 2", advisor: "Prof. Tobias Toll", year: "2021-present", research: "Hadron Properties" },
+        { name: "PhD Scholar 2", advisor: "Prof. Tobias Toll", year: "2021-present", research: "Hadron Properties" },
+        { name: "Pankaj Borah", advisor: "Prof. Pradipta Ghosh", year: "2019-present", research: "Dark Matter, Phase Transition, Gravitational Waves" },
+        { name: "PhD Scholar 2", advisor: "Prof. Suprit Singh", year: "2021-present", research: "Hadron Properties" },
+        { name: "PhD Scholar 2", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Hadron Properties" },
+        { name: "PhD Scholar 2", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Hadron Properties" },
+        { name: "PhD Scholar 2", advisor: "Prof. Tarun Sharma", year: "2021-present", research: "Hadron Properties" },
+        { name: "Arkapal Mondal", advisor: "Prof. Sarthak Parikh", year: "2022-present", research: "AdS/CFT" },
+        { name: "D Chirag", advisor: "Prof. Sarthak Parikh", year: "2024-present", research: "QFT" },
+        { name: "Rishav Saha", advisor: "Prof. Sarthak Parikh", year: "2024-present", research: "QFT" }
+      ]
+    },
+    alumni: {
+      phd: [
+        { name: "PhD Alumni 1", advisor: "Prof. Amruta Mishra", year: "2018-2023", thesis: "Properties of Hadrons in Strong Fields", current: "Postdoc at TIFR" },
+        { name: "PhD Alumni 2", advisor: "Prof. Pradipta Ghosh", year: "2017-2022", thesis: "Dark Matter Detection", current: "Faculty at IISER" }
+      ]
+    }
+  };
