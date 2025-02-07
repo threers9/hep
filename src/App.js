c4f2943d7dbd7fc7f02e3205a5278c248a21e011
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-07 23:30:25 +0530

    Update App.js
    make faculty list into a grid

--- src/App.js
+++ src/App.js
@@ -297,7 +297,7 @@ useEffect(() => {
         </select>
       </div>
 
-      <div className="space-y-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {faculty
           .filter(f => selectedFilter === 'all' || 
             Object.entries(researchAreas).some(([area, data]) => 
