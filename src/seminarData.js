ba124ac6d5e14632227d9a0399ee698b669728d4
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-08 01:02:18 +0530

    Create seminarData.js

---
+++ src/seminarData.js
@@ -0,0 +1,38 @@
+export const seminarData = [
+          {
+            title: "QCD-Gravity double-copy in the Regge regime via shockwave collisions",
+            speaker: "Himanshu Raj",
+            date: "2025-02-12",
+            area: "Stony Brook University, USA"
+          },
+          {
+            title: "Journal club: TBA",
+            speaker: "Amin Nizami",
+            date: "2025-02-10",
+            area: "Ashoka University"
+          },
+          {
+            title: "Reconciling cosmological tensions with inelastic dark matter and dark radiation",
+            speaker: "Satyabrata Mahapatra",
+            date: "2025-01-29",
+            area: "Sungkyunkwan University, South Korea"
+          },
+          {
+            title: "Journal club: Feynman diagrams live in conformal families",
+            speaker: "Siddharth Prabhu",
+            date: "2025-01-27",
+            area: "TIFR, Mumbai"
+          },
+          {
+            title: "Physics beyond standard model (A dark matter story)",
+            speaker: "Disha Bhatia",
+            date: "2025-01-15",
+            area: "Instituto de Fisica, Universidade de Sao Paulo, Brazil"
+          },
+          {
+            title: "Imprints of Early Universe Cosmology on Gravitational Waves",
+            speaker: "Mudit Rai",
+            date: "2025-01-07",
+            area: "Texas A&M University, USA"
+          }
+        ]
