dfddcfdf32ba50f439eb951fc91a3f4032c81f4f
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-08 10:28:28 +0530

    Update membersData.js
    more members

--- src/membersData.js
+++ src/membersData.js
@@ -2,8 +2,8 @@
   export const members = {
     current: {
       postdoc: [
-        { name: "Postdoc 1", year: "2023-present", research: "Beyond Standard Model" },
-        { name: "Postdoc 1", year: "2023-present", research: "Beyond Standard Model" },
+        { name: "Harkirat Singh Sahota", year: "2024-present", research: "Quantum Fields in Curved Spacetimes, Quantum Information" },
+        { name: "Shagun Kaushal", year: "2024-present", research: "Quantum Fields in Curved Spacetimes, Quantum Information" },
         { name: "Postdoc 1", year: "2023-present", research: "Beyond Standard Model" }
       ],
       phd: [
@@ -13,7 +13,7 @@
         { name: "PhD Scholar 2", advisor: "Prof. Tobias Toll", year: "2021-present", research: "Hadron Properties" },
         { name: "PhD Scholar 2", advisor: "Prof. Tobias Toll", year: "2021-present", research: "Hadron Properties" },
         { name: "Pankaj Borah", advisor: "Prof. Pradipta Ghosh", year: "2019-present", research: "Dark Matter, Phase Transition, Gravitational Waves" },
-        { name: "PhD Scholar 2", advisor: "Prof. Suprit Singh", year: "2021-present", research: "Hadron Properties" },
+        { name: "Shivam Burman", advisor: "Prof. Suprit Singh", year: "2021-present", research: "xtragalactic Astrophysics" },
         { name: "PhD Scholar 2", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Hadron Properties" },
         { name: "PhD Scholar 2", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Hadron Properties" },
         { name: "PhD Scholar 2", advisor: "Prof. Tarun Sharma", year: "2021-present", research: "Hadron Properties" },
