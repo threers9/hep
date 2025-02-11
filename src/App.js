42fec794901251a620c9fd3a9d1d18e892e76af6
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-11 19:24:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -278,8 +278,6 @@ useEffect(() => {
           >
             Alumni
           </button>
-        // </div>
-        // <div className="flex justify-center">
           <button
             className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300"
             onClick={() => {
