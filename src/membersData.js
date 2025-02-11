2f8ffd263f7dae81908199b5988800984614b364
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-11 16:40:59 +0530

    Update membersData.js

--- src/membersData.js
+++ src/membersData.js
@@ -7,26 +7,30 @@
         { name: "Nahid Vasim", year: "2023-present", research: "QCD Theory at small-x" }
       ],
       phd: [
-        { name: "PhD Scholar 1", advisor: "Prof. Amruta Mishra", year: "2022-present", research: "Strongly Interacting Matter" },
-        { name: "Manju Soni", advisor: "Prof. Amruta Mishra", year: "2019-present", research: "" },
-        { name: "Arpita Mondal", advisor: "Prof. Amruta Mishra", year: "2019-present", research: "" },
+        { name: "Manju Soni", advisor: "Prof. Amruta Mishra", year: "Jul 2019-present", research: "Meson properties under extreme conditions" },
+        { name: "Arpita Mondal", advisor: "Prof. Amruta Mishra", year: "Jul 2019-present", research: "In-medium properties of strange and charm mesons" },
         { name: "Jaswant Singh", advisor: "Prof. Tobias Toll", year: "2019-present", research: " Small-x Diffraction and Machine Learning " },
         { name: "Pankaj Borah", advisor: "Prof. Pradipta Ghosh", year: "2019-present", research: "Dark Matter, Phase Transition, Gravitational Waves" },
         { name: "Shivam Burman", advisor: "Prof. Suprit Singh", year: "2021-present", research: "Extragalactic Astrophysics" },
         { name: "Abhishek Kumar Singh", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Strongly coupled theories" },
         { name: "Tejaswini Thallapalli", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Applications of ML for particle physics data" },
-        { name: "Vaidehi S Nattoja", advisor: "Prof. Tobias Toll", year: "2022-present", research: "Event Generation for Exclusive Diffraction in Ultra-Peripheral Collisions" },
-        { name: "Arkapal Mondal", advisor: "Prof. Sarthak Parikh", year: "2022-present", research: "AdS/CFT" },
-        { name: "D Chirag", advisor: "Prof. Sarthak Parikh", year: "2024-present", research: "QFT" },
-        { name: "Lingaraj Rout", advisor: "Prof. Tarun Sharma", year: "2024-present", research: "Non relativistic supersymmetric theories" },
+        { name: "Vaidehi S Nattoja", advisor: "Prof. Tobias Toll", year: "Jul 2022-present", research: "Event Generation for Exclusive Diffraction in Ultra-Peripheral Collisions" },
+        { name: "Arkapal Mondal", advisor: "Prof. Sarthak Parikh", year: "Jul 2022-present", research: "AdS/CFT" },
+        { name: "D Chirag", advisor: "Prof. Sarthak Parikh", year: "Jul 2024-present", research: "QFT" },
+        { name: "Lingaraj Rout", advisor: "Prof. Tarun Sharma", year: "Jul 2024-present", research: "Non relativistic supersymmetric theories" },
         { name: "Bhakta Naik", advisor: "Prof. Tobias Toll", year: "2024-present", research: "Small-x diffraction phenomenology and event generation" },
-        { name: "Rishav Saha", advisor: "Prof. Sarthak Parikh", year: "2024-present", research: "QFT" }
+        { name: "Rishav Saha", advisor: "Prof. Sarthak Parikh", year: "Dec 2024-present", research: "QFT" }
       ]
     },
     alumni: {
       phd: [
-        { name: "Arjun Kumar", advisor: "Prof. Tobias Toll", year: "2018-2024", thesis: "Small-x gluon tomography", current: "" },
-        { name: "PhD Alumni 2", advisor: "Prof. Amruta Mishra", year: "2017-2022", thesis: "", current: "" }
+        { name: "Arvind Kumar", advisor: "Prof. Amruta Mishra", year: "Jul 2007-Jun 2011", thesis: "In-medium properties of hadrons", current: "" },
+        { name: "Divakar Pathak", advisor: "Prof. Amruta Mishra", year: "Jul 2011-Oct 2025", thesis: "In medium properties of hadrons in a chiral effective model", current: "" },
+        { name: "Amal Jahan C.S.", advisor: "Prof. Amruta Mishra", year: "Jul 2017-Jun 2023", thesis: "Heavy flavour mesons in hadronic medium", current: "" },
+        { name: "Pallabi Parui", advisor: "Prof. Amruta Mishra", year: "Jul 2018-Apr 2024", thesis: "Spectral properties of hadrons in hot and dense matter", current: "" },
+        { name: "Sourodeep De", advisor: "Prof. Amruta Mishra", year: "Jul 2018-Apr 2024", thesis: "Masses and Decay widths of Heavy flavour meson", current: "" },
+        { name: "Ankit Kumar", advisor: "Prof. Amruta Mishra", year: "Jul 2018-Apr 2024", thesis: "Heavy Quarkonia under extreme conditions", current: "" },
+        { name: "Arjun Kumar", advisor: "Prof. Tobias Toll", year: "2018-2024", thesis: "Small-x gluon tomography", current: "" }
       ]
     }
   };
