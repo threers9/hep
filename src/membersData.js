59111f50ba15e972c2530d2bff3c4ee8c7c56fa0
threers9 <62273722+threers9@users.noreply.github.com>
2025-02-11 12:08:53 +0530

    Update membersData.js

--- src/membersData.js
+++ src/membersData.js
@@ -4,27 +4,28 @@
       postdoc: [
         { name: "Harkirat Singh Sahota", year: "2024-present", research: "Quantum Fields in Curved Spacetimes, Quantum Information" },
         { name: "Shagun Kaushal", year: "2023-present", research: "Quantum Fields in Curved Spacetimes, Quantum Information" },
-        { name: "Nahid Vasim", year: "2024-present", research: "QCD" }
+        { name: "Nahid Vasim", year: "2023-present", research: "QCD Theory at small-x" }
       ],
       phd: [
         { name: "PhD Scholar 1", advisor: "Prof. Amruta Mishra", year: "2022-present", research: "Strongly Interacting Matter" },
         { name: "Manju Soni", advisor: "Prof. Amruta Mishra", year: "2019-present", research: "" },
         { name: "Arpita Mondal", advisor: "Prof. Amruta Mishra", year: "2019-present", research: "" },
+        { name: "Jaswant Singh", advisor: "Prof. Tobias Toll", year: "2019-present", research: " Small-x Diffraction and Machine Learning " },
         { name: "Pankaj Borah", advisor: "Prof. Pradipta Ghosh", year: "2019-present", research: "Dark Matter, Phase Transition, Gravitational Waves" },
         { name: "Shivam Burman", advisor: "Prof. Suprit Singh", year: "2021-present", research: "Extragalactic Astrophysics" },
         { name: "Abhishek Kumar Singh", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Strongly coupled theories" },
         { name: "Tejaswini Thallapalli", advisor: "Prof. Abhishek Iyer", year: "2021-present", research: "Applications of ML for particle physics data" },
-        { name: "Vaidehi S Nattoja", advisor: "Prof. Tobias Toll", year: "2022-present", research: "" },
+        { name: "Vaidehi S Nattoja", advisor: "Prof. Tobias Toll", year: "2022-present", research: "Event Generation for Exclusive Diffraction in Ultra-Peripheral Collisions" },
         { name: "Arkapal Mondal", advisor: "Prof. Sarthak Parikh", year: "2022-present", research: "AdS/CFT" },
         { name: "D Chirag", advisor: "Prof. Sarthak Parikh", year: "2024-present", research: "QFT" },
         { name: "Lingaraj Rout", advisor: "Prof. Tarun Sharma", year: "2024-present", research: "Non relativistic supersymmetric theories" },
-        { name: "Bhakta Naik", advisor: "Prof. Tobias Toll", year: "2024-present", research: "" },
+        { name: "Bhakta Naik", advisor: "Prof. Tobias Toll", year: "2024-present", research: "Small-x diffraction phenomenology and event generation" },
         { name: "Rishav Saha", advisor: "Prof. Sarthak Parikh", year: "2024-present", research: "QFT" }
       ]
     },
     alumni: {
       phd: [
-        { name: "Arjun Kumar", advisor: "Prof. Tobias Toll", year: "2018-2023", thesis: "", current: "" },
+        { name: "Arjun Kumar", advisor: "Prof. Tobias Toll", year: "2018-2024", thesis: "Small-x gluon tomography", current: "" },
         { name: "PhD Alumni 2", advisor: "Prof. Amruta Mishra", year: "2017-2022", thesis: "", current: "" }
       ]
     }
